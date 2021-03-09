e7cb118e73dcf493a43b9e9399f52f0703392ebe
robert <t108408076@ntut.org.tw>
2021-03-09 15:47:47 +0800

    1

--- testfunction.py
+++ testfunction.py
@@ -8,46 +8,57 @@ from scipy.stats import stats
 import pandas as pd
 import numpy as np
 df = pd.read_excel('data.xlsx', index_col=False)
-#種類跟數量
-substance=input("代測物質:")
-substance=df[substance]
+# 種類跟數量
+substance = input("代測物質:")
+substance = df[substance]
+
 
 def normal(value):
-    p=stats.pearsonr(value,substance)
+    p = stats.pearsonr(value, substance)
     return abs(p[0])
+
 
 def ln_normal(value):
-    value=np.log(value)
-    p=stats.pearsonr(value, substance)
+    value = np.log(value)
+    p = stats.pearsonr(value, substance)
     return abs(p[0])
 
-def plus(n1,n2):
-    value=n1+n2
-    p=stats.pearsonr(value, substance)
+
+def plus(n1, n2):
+    value = n1+n2
+    p = stats.pearsonr(value, substance)
     return abs(p[0])
 
-def minus(n1,n2):
-    value=n1-n2
-    p=stats.pearsonr(value, substance)
+
+def minus(n1, n2):
+    value = n1-n2
+    p = stats.pearsonr(value, substance)
     return abs(p[0])
 
-def ln_plus(n1,n2):
-    value=np.log(n1+n2)  
-    p=stats.pearsonr(value, substance)
+
+def ln_plus(n1, n2):
+    value = np.log(n1+n2)
+    p = stats.pearsonr(value, substance)
     return abs(p[0])
 
-def ln_minus(n1,n2):
-    value=np.log(n1-n2+2)  #+2的原因是怕相減為負數無法進行對數運算
-    p=stats.pearsonr(value, substance)
+
+def ln_minus(n1, n2):
+    value = np.log(n1-n2+2)  # +2的原因是怕相減為負數無法進行對數運算
+    p = stats.pearsonr(value, substance)
     return abs(p[0])
 
-def divide(n1,n2):
-    value=n1/n2  
-    p=stats.pearsonr(value, substance)
+
+def divide(n1, n2):
+    value = n1/n2
+    p = stats.pearsonr(value, substance)
     return abs(p[0])
 
-def ln_divide(n1,n2):
-    value=np.log(n1/n2)  
-    p=stats.pearsonr(value, substance)
+
+def ln_divide(n1, n2):
+    value = np.log(n1/n2)
+    p = stats.pearsonr(value, substance)
     return abs(p[0])
 
+
+if __name__ == '__main__':
+    main()
